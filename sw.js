017d4174b36a5897c9d5e606351f858aeaccac55
Double A <208642327+AntManThePro@users.noreply.github.com>
2026-08-31 20:15:04 -0500

    Migrate to TypeScript: app-core.ts + app-ui.ts, strict types, module scripts, package.json + tsconfig

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-const CACHE = "cycle-gate-v2";
-const ASSETS = ["./index.html", "./manifest.json", "./icons/icon.svg", "./app-core.js", "./app-ui.js"];
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
-  );
-});
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
-});
+sw_cycle
